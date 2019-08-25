e595c27d7f84c43fd9f551f973659e959e48b5cd
Bunbury <joseabel.ramirezfrontany@gmail.com>
2019-08-24 22:42:14 -0400

    Adding new information to the forecast string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -16,7 +16,7 @@ const forecast = (latitude, longitude, callback) => {
 
         }else {
             
-            callback(undefined, body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees out. There is " + body.currently.precipProbability + " % chance of rain")
+            callback(undefined, body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees out. There is " + body.currently.precipProbability + " % chance of rain today. " + body.daily.summary + " The visibility of our beautiful sky is of" + " " + body.currently.visibility + ".")
         }
     })
 }
